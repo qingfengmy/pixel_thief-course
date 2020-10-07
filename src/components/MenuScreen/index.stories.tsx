9162de95fb4856902b7ff07e076dacf1efc00f35
Brad Woods <brad@easyagile.com>
2020-10-07 17:27:22 +1100

    Add menu screen

---
+++ src/components/MenuScreen/index.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { MenuScreen } from ".";
+import { Meta } from "@storybook/react/types-6-0";
+
+export default {
+  title: `Small Components/MenuScreen`,
+} as Meta;
+
+export const Index = () => <MenuScreen>Children placeholder</MenuScreen>;
