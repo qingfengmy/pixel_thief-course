4c4a3df26178df3c63ee11043b8375432519b6e9
Brad Woods <brad@easyagile.com>
2020-10-11 09:39:55 +1100

    Add levels placeholders to game component

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -14,13 +14,35 @@ export const Game = () => {
   }
 
   if (state.matches("playing")) {
-    return (
-      <>
-        <p>state: playing</p>
-        <Button onClick={() => send("PLAYER_DIED")}>PLAYER_DIED</Button>
-        <Button onClick={() => send("PLAYER_GOT_TREASURE")}>PLAYER_GOT_TREASURE</Button>
-      </>
-    );
+    if (state.matches("playing.level1")) {
+      return (
+        <>
+          <p>state: playing.level1</p>
+          <Button onClick={() => send("PLAYER_DIED")}>PLAYER_DIED</Button>
+          <Button onClick={() => send("PLAYER_WALKED_THROUGH_DOOR")}>PLAYER_WALKED_THROUGH_DOOR</Button>
+        </>
+      );
+    }
+
+    if (state.matches("playing.level2")) {
+      return (
+        <>
+          <p>state: playing.level2</p>
+          <Button onClick={() => send("PLAYER_DIED")}>PLAYER_DIED</Button>
+          <Button onClick={() => send("PLAYER_WALKED_THROUGH_DOOR")}>PLAYER_WALKED_THROUGH_DOOR</Button>
+        </>
+      );
+    }
+
+    if (state.matches("playing.level3")) {
+      return (
+        <>
+          <p>state: playing.level3</p>
+          <Button onClick={() => send("PLAYER_DIED")}>PLAYER_DIED</Button>
+          <Button onClick={() => send("PLAYER_GOT_TREASURE")}>PLAYER_GOT_TREASURE</Button>
+        </>
+      );
+    }
   }
 
   if (state.matches("gameOver")) {
