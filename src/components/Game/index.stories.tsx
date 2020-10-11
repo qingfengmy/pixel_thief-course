3b51c4d8bc895e5d47a4c869eff93192fda3d2fe
Brad Woods <brad@easyagile.com>
2020-10-11 14:59:39 +1100

    Add fastForward events

--- src/components/Game/index.stories.tsx
+++ src/components/Game/index.stories.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 import { Game } from ".";
 import { Meta } from "@storybook/react/types-6-0";
+import {
+    fastForwardToLevel1Events,
+    fastForwardToLevel2Events,
+    fastForwardToLevel3Events,
+    fastForwardToGameCompleteEvents,
+    fastForwardToGameOverEvents,
+} from "./fastForwardEvents";
 
 export default {
-  title: `Game`,
+    title: `Game`,
 } as Meta;
 
 export const Index = () => <Game />;
+
+export const Level1 = () => (
+    <Game fastForwardEvents={fastForwardToLevel1Events} />
+);
+
+export const Level2 = () => (
+    <Game fastForwardEvents={fastForwardToLevel2Events} />
+);
+
+export const Level3 = () => (
+    <Game fastForwardEvents={fastForwardToLevel3Events} />
+);
+
+export const GameComplete = () => (
+    <Game fastForwardEvents={fastForwardToGameCompleteEvents} />
+);
+
+export const GameOver = () => (
+    <Game fastForwardEvents={fastForwardToGameOverEvents} />
+);
--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,57 +1,95 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { gameMachine } from "../../machines/gameMachine";
 import { useMachine } from "@xstate/react";
 import { Button } from "../Button";
 import { HomeScreen } from "../HomeScreen";
 import { GameOverScreen } from "../GameOverScreen";
 import { GameCompleteScreen } from "../GameCompleteScreen";
+import { GameEventType } from "../../machines/gameMachine/types";
 
-export const Game = () => {
-  const [state, send] = useMachine(gameMachine);
+interface PropsType {
+    fastForwardEvents?: GameEventType[];
+}
 
-  if (state.matches("home")) {
-    return <HomeScreen onStartButtonClick={() => send("START_BUTTON_CLICKED")} />;
-  }
+export const Game = ({ fastForwardEvents }: PropsType) => {
+    const [state, send] = useMachine(gameMachine);
 
-  if (state.matches("playing")) {
-    if (state.matches("playing.level1")) {
-      return (
-        <>
-          <p>state: playing.level1</p>
-          <Button onClick={() => send("PLAYER_DIED")}>PLAYER_DIED</Button>
-          <Button onClick={() => send("PLAYER_WALKED_THROUGH_DOOR")}>PLAYER_WALKED_THROUGH_DOOR</Button>
-        </>
-      );
+    useEffect(() => {
+        // note: batching events doesn't work at time of writing
+        if (fastForwardEvents) {
+            fastForwardEvents.forEach((event: GameEventType) => {
+                send(event);
+            });
+        }
+    }, [fastForwardEvents, send]);
+
+    if (state.matches("home")) {
+        return (
+            <HomeScreen
+                onStartButtonClick={() => send("START_BUTTON_CLICKED")}
+            />
+        );
     }
 
-    if (state.matches("playing.level2")) {
-      return (
-        <>
-          <p>state: playing.level2</p>
-          <Button onClick={() => send("PLAYER_DIED")}>PLAYER_DIED</Button>
-          <Button onClick={() => send("PLAYER_WALKED_THROUGH_DOOR")}>PLAYER_WALKED_THROUGH_DOOR</Button>
-        </>
-      );
+    if (state.matches("playing")) {
+        if (state.matches("playing.level1")) {
+            return (
+                <>
+                    <p>state: playing.level1</p>
+                    <Button onClick={() => send("PLAYER_DIED")}>
+                        PLAYER_DIED
+                    </Button>
+                    <Button onClick={() => send("PLAYER_WALKED_THROUGH_DOOR")}>
+                        PLAYER_WALKED_THROUGH_DOOR
+                    </Button>
+                </>
+            );
+        }
+
+        if (state.matches("playing.level2")) {
+            return (
+                <>
+                    <p>state: playing.level2</p>
+                    <Button onClick={() => send("PLAYER_DIED")}>
+                        PLAYER_DIED
+                    </Button>
+                    <Button onClick={() => send("PLAYER_WALKED_THROUGH_DOOR")}>
+                        PLAYER_WALKED_THROUGH_DOOR
+                    </Button>
+                </>
+            );
+        }
+
+        if (state.matches("playing.level3")) {
+            return (
+                <>
+                    <p>state: playing.level3</p>
+                    <Button onClick={() => send("PLAYER_DIED")}>
+                        PLAYER_DIED
+                    </Button>
+                    <Button onClick={() => send("PLAYER_GOT_TREASURE")}>
+                        PLAYER_GOT_TREASURE
+                    </Button>
+                </>
+            );
+        }
     }
 
-    if (state.matches("playing.level3")) {
-      return (
-        <>
-          <p>state: playing.level3</p>
-          <Button onClick={() => send("PLAYER_DIED")}>PLAYER_DIED</Button>
-          <Button onClick={() => send("PLAYER_GOT_TREASURE")}>PLAYER_GOT_TREASURE</Button>
-        </>
-      );
+    if (state.matches("gameOver")) {
+        return (
+            <GameOverScreen
+                onRestartButtonClick={() => send("RESTART_BUTTON_CLICKED")}
+            />
+        );
     }
-  }
 
-  if (state.matches("gameOver")) {
-    return <GameOverScreen onRestartButtonClick={() => send("RESTART_BUTTON_CLICKED")} />;
-  }
+    if (state.matches("gameComplete")) {
+        return (
+            <GameCompleteScreen
+                onGoHomeButtonClick={() => send("HOME_BUTTON_CLICKED")}
+            />
+        );
+    }
 
-  if (state.matches("gameComplete")) {
-    return <GameCompleteScreen onGoHomeButtonClick={() => send("HOME_BUTTON_CLICKED")} />;
-  }
-
-  throw Error(`Unknown game state: ${state.value}`);
+    throw Error(`Unknown game state: ${state.value}`);
 };
