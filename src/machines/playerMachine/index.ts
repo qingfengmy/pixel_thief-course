586b8e3a6537913926b04381f6083c310e49fb62
Brad Woods <brad@easyagile.com>
2020-10-13 18:59:46 +1100

    Add player movement

--- src/machines/playerMachine/index.ts
+++ src/machines/playerMachine/index.ts
@@ -1,26 +1,72 @@
 import { createMachine } from "xstate";
 import { PLAYER_STARTING_COORDS } from "../../constants";
-import { log } from "xstate/lib/actions";
-import { PlayerContextType, PlayerEventType, PlayerStateType } from "./types";
+import { assign, choose } from "xstate/lib/actions";
+import { getTargetCoords } from "../../util/getTargetCoords";
+import { isCoordsOnGrid } from "../../util/isCoordsOnGrid";
+import {
+    ArrowButtonClickedType,
+    PlayerContextType,
+    PlayerEventType,
+    PlayerStateType,
+} from "./types";
 
 export const playerMachine = createMachine<
     PlayerContextType,
     PlayerEventType,
     PlayerStateType
->({
-    context: {
-        coords: PLAYER_STARTING_COORDS,
-    },
-    id: `player`,
-    initial: `alive`,
-    states: {
-        alive: {
-            on: {
-                ARROW_BUTTON_CLICKED: {
-                    actions: log(),
+>(
+    {
+        context: {
+            coords: PLAYER_STARTING_COORDS,
+        },
+        id: `player`,
+        initial: `alive`,
+        states: {
+            alive: {
+                on: {
+                    ARROW_BUTTON_CLICKED: {
+                        actions: `onArrowButtonClick`,
+                    },
                 },
             },
+            dead: {},
         },
-        dead: {},
     },
-});
+    {
+        actions: {
+            onArrowButtonClick: choose([
+                {
+                    cond: `isSquareAvailable`,
+                    actions: `move`,
+                },
+            ]),
+            move: assign<PlayerContextType, ArrowButtonClickedType>(
+                (context, event) => {
+                    const { coords } = context;
+                    const { direction } = event;
+                    const targetCoords = getTargetCoords({ coords, direction });
+
+                    return {
+                        coords: targetCoords,
+                    };
+                }
+            ) as any,
+        },
+        guards: {
+            isSquareAvailable: (
+                context: PlayerContextType,
+                event: PlayerEventType
+            ) => {
+                if (event.type === "ARROW_BUTTON_CLICKED") {
+                    const { coords } = context;
+                    const { direction } = event;
+                    const targetCoords = getTargetCoords({ coords, direction });
+
+                    return isCoordsOnGrid(targetCoords);
+                }
+
+                return false;
+            },
+        },
+    }
+);
